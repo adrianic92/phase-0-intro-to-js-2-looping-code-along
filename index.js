579ec84a16b7b1695418c368421ca6a7442d4506
Adrian <adrianic92@yahoo.com>
2022-07-09 14:14:47 -0700

    commit tree dbfc37bc0156aa38e9577936c9cf402ad918d342

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    const newArray = [];
+    for (let counter = 0; counter < array.length; counter++) {
+        newArray[counter] = `Thank you, ${array[counter]}, for the wonderful ${event} gift!`;
+    }
+    return newArray;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
